7b916d1a2190e33457af9377a809ea49d787ec9f
estarhp <112890331+estarhp@users.noreply.github.com>
2023-03-21 22:27:46 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include<stdio.h>
-int main(int argc, char const *argv[])
-{
+#include "stdlib.h"
 
-    
-    /* code */
+void  ShowMainMenu(){
+    system("cls");
+
+    printf("\n\t##########################欢迎进入##########################\t\n"
+                  "\n\t                        1->图书管理                         \t\n"
+                  "\n\t                        2->读者管理                         \t\n"
+                  "\n\t                        3->借还书登记                       \t\n"
+                  "\n\t#######################请数字选择进入########################\t\n"
+                  );
+
+
+
+
+}
+
+
+
+int main(int argc, char const *argv[])
+{   int Scan_number=0;
+
+
+    ShowMainMenu();
+
+
+
+    scanf("%d",&Scan_number);
+
     return 0;
 }
