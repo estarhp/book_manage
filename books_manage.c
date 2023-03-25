bc71b8639f7104637eaaae9f53ddbb692ce3a967
Vinci230 <2798380278@qq.com>
2023-03-25 16:20:32 +0800

    交换name和writer

--- books_manage.c
+++ books_manage.c
@@ -54,7 +54,7 @@ void init_book(int number,FILE *fp,Book books[]){
 
 
     for (int i = 0; i < number; ++i) {
-        scanf("%d %s %s",&books[i].id,&books[i].name,&books[i].writer);
+        scanf("%d %s %s",&books[i].id,&books[i].writer,&books[i].name);
     }//从文件读入书本
 
     fclose(fp);
