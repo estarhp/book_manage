a841cd6f0b8a3cdb1b0c8e91a3c056801acbff93
estarhp <112890331+estarhp@users.noreply.github.com>
2023-03-31 17:05:24 +0800

    updata:show_book

--- book_manage/books_manage.c
+++ book_manage/books_manage.c
@@ -52,7 +52,14 @@ void Main_book_mana(){
 }
 
 void show_book(int books_number,Book *books){
-    for (int i = 0; i < books_number; ++i) {
+    int start=0,end=0;
+    printf("请输入要看的书本的序数范围：\n从序数__开始：");
+    scanf("%d",&start);
+    printf("从序数__结束：");
+    scanf("%d",&end);
+    printf("以下为书本序数在%d~%d范围之内的图书：\n",start,end);
+
+    for (int i = start; i <= end; ++i) {
         printf("%d %s %s\n",books[i].id,books[i].writer,books[i].name);
     }
 
