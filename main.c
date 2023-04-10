56e967831ee19670c22ba81232d76923c33db74c
Vinci230 <2798380278@qq.com>
2023-04-10 19:44:13 +0800

    新菜单光标

--- main.c
+++ main.c
@@ -1,106 +1,111 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "stdlib.h"
+
 #include <stdio.h>
 #include <windows.h>
-#include <conio.h>
 #include "book_manage/book_manage.h"
 #include "reader_manage/reader_manage.h"
 #include "borrow_return/borrow_return.h"
-#define N 1024
+
 void Collect_operation();
-void printMenu();
-void Move(char dir);
 //定位箭头
-static COORD coord;
-void Move(char dir)
-{
-    COORD originCoord = coord;
-    if (dir == 'w' && coord.Y <= 6&& coord.Y > 2)
-    {
-        coord.Y--;
-    }
-    else if (dir == 's' && coord.Y < 6 && coord.Y >= 2)
-    {
-        coord.Y++;
-    }
+int wherex()
 
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), originCoord);
-    printf("   ");
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-    printf("==>");
+{
+
+    CONSOLE_SCREEN_BUFFER_INFO pBuffer;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &pBuffer);
+
+    return (pBuffer.dwCursorPosition.X + 1);
 
 }
-static char menuContent[8][N] = {
-        {"********                图书管理系统                ********"},
-        {"##########################欢迎进入##########################"},
-        {"#                        0->谢谢使用                       #"},
-        {"#                        1->图书管理                       #"},
-        {"#                        2->读者管理                       #"},
-        {"#                        3->借还书登记                     #"},
-        {"#                        其他->请重输                      #"},
-        {"#######################请选择数字进入#######################"}
-};
-void printMenu() {
-    //system("cls")
-    coord.X = 24;//列
-    coord.Y = 0;//行
-    for (int i = coord.Y, j = 0; i <= 9; i++)
-    {
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-        coord.Y++;
-        printf("%s\n", menuContent[j++]);
-    }
 
-    coord.X = 20;
-    coord.Y = 2;//行数从哪儿开始
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-    printf("==>");
+//获取光标的位置y
+
+int wherey()
+
+{
+
+    CONSOLE_SCREEN_BUFFER_INFO pBuffer;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &pBuffer);
+
+    return (pBuffer.dwCursorPosition.Y + 1);
+
 }
 
+//设置光标的位置
 
+void gotoxy(int x, int y)
 
-int main(int argc, char const *argv[])
 {
-    //打印窗口
-    //ShowMainMenu();
-   //收集输入的数字
 
+    COORD c;
 
-    printMenu();
+    c.X = x - 1;
 
+    c.Y = y - 1;
 
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 
+}
 
-    while (1)
-    {
-        if (_kbhit())
-        {
-            char Direction = _getch();
-            if (Direction == 'w' || Direction == 's' || Direction == '1' || Direction == '2' || Direction == '3')
-            {
-                Move(Direction);
-            }
-        }
-    }
+int main()
+
+{
+
+    setbuf(stdout,0);
+
+    int x, y;
+
+    //int select;
+
+    gotoxy(25, 5);
+
+    printf("********图书管理系统********");
+
+    gotoxy(15, 8);
+
+    printf("*********************主菜单*********************");
+
+    gotoxy(15, 9);
+
+    printf("*                  0->谢谢使用                 *");
+
+    gotoxy(15, 10);
+
+    printf("*                  1->图书管理                 *");
+
+    gotoxy(15, 11);
+
+    printf("*                  2->读者管理                 *");
+
+    gotoxy(15, 12);
+
+    printf("*                  3->借还书登记               *");
+
+    gotoxy(15, 13);
+
+    printf("*                  其他->请重输                *");
+
+    gotoxy(15, 14);
+
+    printf("************************************************");
+
+    gotoxy(28, 16);
+
+    printf("请输入你的选择(数字):[ ]");
+
+    x = wherex();
+
+    y = wherey();
+
+    gotoxy(x - 2, y);
+
+    //scanf_s("%d", &select);
     Collect_operation();
     return 0;
 
-
 }
-
-/*void  ShowMainMenu() {
-
-    printf("\n\t********                图书管理系统                ********\t\n");
-    printf(
-            "\n\t##########################欢迎进入##########################\t"
-            "\n\t#                        0->谢谢使用                       #\t"
-            "\n\t#                        1->图书管理                       #\t"
-            "\n\t#                        2->读者管理                       #\t"
-            "\n\t#                        3->借还书登记                     #\t"
-            "\n\t#                        其他->请重输                      #\t"
-            "\n\t#######################请数字选择进入########################\t"
-    );
-}*/
 void Collect_operation(){
     int Scan_number=0;
 
