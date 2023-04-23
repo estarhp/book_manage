be783d1da77f5b0d882a449cc7544014f75c6015
Vinci230 <2798380278@qq.com>
2023-04-23 19:27:56 +0800

    图书管理菜单

--- book_manage/books_manage.c
+++ book_manage/books_manage.c
@@ -14,13 +14,13 @@ void put_book(int number,FILE *fp,Book books[]);//初始化图书
 int Books_number(FILE *fp);//计算文件中图书的数目
 void Menu_book();
 void Collect(int books_number,Book books[],FILE *fp);//收集用户的选择
-void delete_book();
+void delete_book();//删除
 void modify_book();
 void query_book(int books_number,Book books[]);//查询图书
 void show_book(int books_number,Book *books);//展示图书
-void query_byid(int books_number,Book books[]);
-void query_bywriter(int books_number,Book books[]);
-void query_bybookname(int books_number,Book books[]);
+void query_byid(int books_number,Book books[]);//通过id
+void query_bywriter(int books_number,Book books[]);//通过作者
+void query_bybookname(int books_number,Book books[]);//通过书名
 int wherex_book();
 int wherey_book();
 void gotoxy_book(int x, int y);
