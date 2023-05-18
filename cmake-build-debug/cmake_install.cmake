e8df1e97674e9c516e6ab892fa653604c006d720
estarhp <112890331+estarhp@users.noreply.github.com>
2023-05-18 11:54:43 +0800

    Revert "update"
    This reverts commit dbee1a77f5eacd6a12d30b7df8e1dca71dc52113.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Users/Vinci/Desktop/book_manage
+# Install script for directory: D:/Desktop/book-manage
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/CLion 2023.1.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Users/Vinci/Desktop/book_manage/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Desktop/book-manage/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
